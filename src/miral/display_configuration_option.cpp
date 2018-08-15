ee1ce090789ede4b42b68a7ce1f28a0f91835045
Alan Griffiths <alan@octopull.co.uk>
2018-08-15 14:00:54 +0100

    Dump display outputs to log

--- src/miral/display_configuration_option.cpp
+++ src/miral/display_configuration_option.cpp
@@ -22,6 +22,9 @@
 #include <mir/graphics/display_configuration.h>
 #include <mir/options/option.h>
 #include <mir/server.h>
+#include <mir_toolkit/mir_client_library.h>
+#include <iostream>
+#include <map>
 
 namespace mg = mir::graphics;
 
@@ -67,9 +70,84 @@ PixelFormatSelector::PixelFormatSelector(
 void PixelFormatSelector::apply_to(mg::DisplayConfiguration& conf)
 {
     base_policy->apply_to(conf);
+
+    std::map<std::string, int> counts;
+
     conf.for_each_output(
         [&](mg::UserDisplayConfigurationOutput& conf_output)
         {
+//
+//            std::cout
+//                << "/----------------------------------------------------------------------------\\\n";
+//            std::cout << "{\n\tid: " << conf_output.id << "\n\tcard_id: " << conf_output.card_id
+//                << "\n\ttype: " << mir_output_type_name(static_cast<MirOutputType>(conf_output.type))
+//                << "\n\tmodes: [ ";
+//
+//            for (size_t i = 0; i < conf_output.modes.size(); ++i)
+//            {
+//                std::cout << conf_output.modes[i];
+//                if (i != conf_output.modes.size() - 1)
+//                    std::cout << ", ";
+//            }
+//
+//            std::cout << "]" << std::endl;
+//            std::cout << "\tpreferred_mode: " << conf_output.preferred_mode_index << std::endl;
+//            std::cout << "\tphysical_size_mm: " << conf_output.physical_size_mm.width << "x" << conf_output.physical_size_mm.height << std::endl;
+//            std::cout << "\tconnected: " << (conf_output.connected ? "true" : "false") << std::endl;
+//            std::cout << "\tused: " << (conf_output.used ? "true" : "false") << std::endl;
+//            std::cout << "\ttop_left: " << conf_output.top_left << std::endl;
+//            std::cout << "\tcurrent_mode: " << conf_output.current_mode_index << " (";
+//            if (conf_output.current_mode_index < conf_output.modes.size())
+//                std::cout << conf_output.modes[conf_output.current_mode_index];
+//            else
+//                std::cout << "none";
+//            std::cout << ")" << std::endl;
+//
+//            std::cout << "\tscale: " << conf_output.scale << std::endl;
+//            std::cout << "\tform factor: ";
+//            switch (conf_output.form_factor)
+//            {
+//            case mir_form_factor_monitor:
+//                std::cout << "monitor";
+//                break;
+//            case mir_form_factor_projector:
+//                std::cout << "projector";
+//                break;
+//            case mir_form_factor_tv:
+//                std::cout << "TV";
+//                break;
+//            case mir_form_factor_phone:
+//                std::cout << "phone";
+//                break;
+//            case mir_form_factor_tablet:
+//                std::cout << "tablet";
+//                break;
+//            default:
+//                std::cout << "UNKNOWN";
+//            }
+//            std::cout <<  std::endl;
+//
+//            std::cout << "\tcustom logical size: ";
+//            if (conf_output.custom_logical_size.is_set())
+//            {
+//                auto const& size = conf_output.custom_logical_size.value();
+//                std::cout << size.width.as_int() << "x" << size.height.as_int();
+//            }
+//            else
+//            {
+//                std::cout << "not set";
+//            }
+//            std::cout << std::endl;
+//
+//            std::cout << "\torientation: " << conf_output.orientation << '\n';
+//            std::cout << "}" << std::endl;
+//            std::cout << "\\----------------------------------------------------------------------------/\n";
+
+            auto const type = mir_output_type_name(static_cast<MirOutputType>(conf_output.type));
+
+            std::cout << "Output:\tcard_id: " << conf_output.card_id << "\tid: " << conf_output.id
+                      << "\ttype: " << type << "\tcount: " << ++counts[type] << '\n';
+
             if (!conf_output.connected || !conf_output.used) return;
 
             auto const& pos = find_if(conf_output.pixel_formats.begin(),
